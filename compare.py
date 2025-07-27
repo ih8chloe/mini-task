55069a178abd1d157e48e5f4a8d445e762cddf99
Ckyle Tagsip <ckyletagsip@Ckyles-MacBook-Air.local>
2025-07-27 13:17:29 +0800

    modified code

--- compare.py
+++ compare.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv('datasets/order_kpi_feed.csv')
-print(df.to_string())
+df1 = pd.read_csv('datasets/order_kpi_feed.csv')
+df2 = pd.read_csv('datasets/orders_raw.csv')
+
+newdf = pd.merge(df1, df2, how="outer", indicator=True)
+discrepancy = newdf.loc[newdf['_merge'] == 'right_only']
+
+print(discrepancy)
